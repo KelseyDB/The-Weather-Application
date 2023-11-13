c8743f5b39c1f61e7b9547ac09642f054877e01b
Kelsey De Busschere <kelseydebusschere@gmail.com>
2023-11-13 10:38:17 +0100

    Add basic html structure and files

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+//TODO: create homepage, submit box to enter city
+//TODO: on clicking submit or pressing ENTER: 
+//      -use API to define city, geo location data from use input
+//      -use API to get weather data for next 5 days, based on geo location data
+//      -manipulate DOM to display weather data
+//TODO: make APIs asynchronous
+//TODO: RESPONSIVE, ACCESSIBLE, MOBILE FRIENDLY
