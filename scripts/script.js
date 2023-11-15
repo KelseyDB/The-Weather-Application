28bb3827d86773c71b25cde00ac5275839a8b082
Kelsey De Busschere <kelseydebusschere@gmail.com>
2023-11-15 10:12:09 +0100

    Add removechildren to find new cities

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,8 @@
 
 const cityInput = document.querySelector("#cityName");
 const button = document.querySelector(".cityButton");
-
 const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 const container = document.querySelector(".card-container")
-
-const newDate = new Date('2023-11-14')
-// to get today
-
-const dayOfTheWeek = newDate.getDay()
-
-console.log(dayOfTheWeek)
-//the output here would be 2
-//it is calculated with sunday === 0 and saturday === 6
-
 
 //get geo data from api with function to usein start weather
 async function getGeoData(city){
@@ -24,7 +13,7 @@ async function getGeoData(city){
 }
 //get weather data based on lat, long from geo data,
 async function getWeatherData(location){
-  const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude="+location.latitude +"&longitude="+location.longitude+"&hourly=is_day&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=GMT")
+  const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude="+location.latitude +"&longitude="+location.longitude+"&current=is_day&hourly=is_day&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=GMT")
   const data = await response.json();
   return data;
 }
@@ -35,57 +24,74 @@ async function startWeather(){
   const cityName = document.querySelector("#cityName").value;
   const geoData = await getGeoData(cityName)
   const weatherData = await getWeatherData(geoData);
-  console.log(weatherData.daily);
+  console.log(weatherData);
+
+  //while loop to remove children
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
 
   //for loop to create cards with info from weatherData
   for (let i = 0; i <= 6; i++) {
     const card = document.createElement('div');
-    container.appendChild(card)
 
     const weekday = document.createElement('p');
     const day = new Date(weatherData.daily.time[i])
+    weekday.className = "weekday"
     weekday.textContent = daysOfTheWeek[day.getDay()]
-    weekday.className = "date"
-    card.append(weekday)
+    weekday.className = "day"
 
     const image = document.createElement('img');
-    const weatherCode = weatherData.daily.weather_code[i]
-      {if (weatherCode == 0) {
-        image.src = "./images/WeatherCode/day-code-0.png"
-      }
-      if (weatherCode == 1 || weatherCode == 2){
-        image.src = "./images/WeatherCode/day-code-1-2.png"
-      }
-      if (weatherCode == 3) {
-        image.src = "./images/WeatherCode/code-3.png"
-      }
-      if (51 <= weatherCode && weatherCode <= 67) {
-        image.src = "./images/WeatherCode/day-code-51-67.png"        
-      }
-      if (71 <= weatherCode && weatherCode <= 77) {
-        image.src = "./images/WeatherCode/code-71-77.png"        
-      }
-      if (80 <= weatherCode && weatherCode <= 82) {
-        image.src = "./images/WeatherCode/code-80-82.png"        
-      }
-      if (85 <= weatherCode && weatherCode <= 86) {
-        image.src = "./images/WeatherCode/code-85-86.png"        
-      }
-      if (95<= weatherCode && weatherCode <= 99) {
-        image.src = "./images/WeatherCode/code-95-99.png"        
-      }
-      }
-    console.log(weatherCode);
-    card.append(image)
-
     const temperature = document.createElement('div');
     const tempMax = document.createElement('p');
     const tempMin = document.createElement('p');
-    tempMax.textContent = weatherData.daily.temperature_2m_max[i];
-    tempMin.textContent = weatherData.daily.temperature_2m_min[i];
-    temperature.appendChild(tempMax);
-    temperature.appendChild(tempMin);
+
+    const weatherCode = weatherData.daily.weather_code[i]
+    const isDay = weatherData.current.is_day
+    console.log(isDay);
+      {if (weatherCode == 0 && isDay == 1) {
+          image.src = "./images/WeatherCode/day-code-0.png"
+        }if (weatherCode == 0 && isDay == 0) {
+          image.src = "./images/WeatherCode/night-code0.png"
+        }
+        if (weatherCode == 1 || weatherCode == 2 && isDay == 1){
+          image.src = "./images/WeatherCode/day-code-1-2.png"
+        }if (weatherCode == 1 || weatherCode == 2 && isDay == 0){
+          image.src = "./images/WeatherCode/night-code1-2.png"
+        }
+        if (weatherCode == 3) {
+          image.src = "./images/WeatherCode/code-3.png"
+        }
+        if (51 <= weatherCode && weatherCode <= 67 && isDay == 1) {
+          image.src = "./images/WeatherCode/day-code-51-67.png"        
+        }if (51 <= weatherCode && weatherCode <= 67 && isDay == 0) {
+          image.src = "./images/WeatherCode/night-code-51-67.png"        
+        }
+        if (71 <= weatherCode && weatherCode <= 77) {
+          image.src = "./images/WeatherCode/code-71-77.png"        
+        }
+        if (80 <= weatherCode && weatherCode <= 82) {
+          image.src = "./images/WeatherCode/code-80-82.png"        
+        }
+        if (85 <= weatherCode && weatherCode <= 86) {
+          image.src = "./images/WeatherCode/code-85-86.png"        
+        }
+        if (95<= weatherCode && weatherCode <= 99) {
+          image.src = "./images/WeatherCode/code-95-99.png"        
+        }
+      }
+
+    card.className = "card"
+    image.className = "weather-image"
+    temperature.className = "temperature"
+    tempMax.textContent = (weatherData.daily.temperature_2m_max[i]+"°C");
+    tempMin.textContent = (weatherData.daily.temperature_2m_min[i]+"°C");
+    container.appendChild(card)
+    card.append(weekday)
+    card.append(image)
     card.appendChild(temperature);
+    temperature.appendChild(tempMin);
+    temperature.appendChild(tempMax);
   }
 }
 //add eventlistener for enter and click on button
